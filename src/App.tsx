a2389806c464df5723cb6e25a65c4c63ffc108bd
ericardmatosz <ericardmatosz@gmail.com>
2023-08-05 01:46:51 -0300

    feature: 'sidebar' component development.
    configurations were also added for importing phosphor-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import '../src/styles/global.scss';
 import '../src/styles/colors.scss'
+import style from './App.module.scss'
+
 import { Header } from './components/header/Header';
+import { Sidebar } from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={style.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
     </div>
   )
 }
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,23 @@
+import style from './Sidebar.module.scss';
+import { PencilSimpleLine } from 'phosphor-react';
+
+export function Sidebar (){
+    return(
+        <aside className={style.sidebar}>
+            <img src="https://images.unsplash.com/photo-1520052205864-92d242b3a76b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
+        
+            <div className={style.userProfile}>
+                <img src='https://github.com/ericardmatosz.png'/>
+                <strong>Érica Matos</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer className={style.editButton}>
+                <a href='#'>
+                    <PencilSimpleLine size={17}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
