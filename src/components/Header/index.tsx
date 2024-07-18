aba8423f7542ba54bccd5224fbf6f2920523c79b
ericardmatosz <ericardmatosz@gmail.com>
2024-07-18 20:02:46 -0300

    refactor: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import igniteLogo from '../../assets/ignite-simbol.svg';
-import style from './Header.module.scss';
+import igniteLogo from "../../assets/ignite-simbol.svg";
+import style from "./styles.module.scss";
 
 export function Header() {
-    return (
-      <header className={style.header}>
-          <img src={igniteLogo} alt=''/>
-          <strong>Ignite Feed</strong> 
-      </header>
-    );
+  return (
+    <header className={style.header}>
+      <img src={igniteLogo} alt="" />
+      <strong>Ignite Feed</strong>
+    </header>
+  );
 }
